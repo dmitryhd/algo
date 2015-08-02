94b6ce4aad6d4bfd8c0e3267922fabb3eea9baa5
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-08-02 23:42:34 +0300

    Heap structure implementation. Heapsort works.

--- struct/heap.py
+++ struct/heap.py
@@ -10,7 +10,7 @@ class Heap(object):
     Used in heap-sort max-heap and in priority queue: min-heap
     """
 
-    def set_arr(self, arr):
+    def __init__(self, arr):
         self.arr = arr
         self.heap_size = len(arr)
 
@@ -56,10 +56,18 @@ class Heap(object):
             self._max_heapify(largest)
 
     def build_heap(self):
-        """ Build heap in self.arr. Time: O(n)  """
+        """ Build heap in self.arr. Time: O(n). """
 
         for ind in reversed(range(math.floor(self.heap_size / 2))):
             self._max_heapify(ind)
+
+    def sort(self):
+        """ Sort heap in self.arr. Time: O(n*ln(n)). """
+        self.build_heap()
+        for ind in reversed(range(1, self.heap_size)):
+            self.arr[0], self.arr[ind] = self.arr[ind], self.arr[0]
+            self.heap_size -= 1
+            self._max_heapify(0)
 
 
 import unittest
@@ -68,23 +76,28 @@ import unittest
 class TestHeap(unittest.TestCase):
     def test_jumps(self):
         """ TestHeap: basic index operations. """
-        heap = Heap()
+        heap = Heap([])
         self.assertEqual(heap._parent(10), 4)
         self.assertEqual(heap._left(10), 21)
         self.assertEqual(heap._right(10), 22)
 
     def test_max_heapify(self):
         """ TestHeap: maintaining of basic heap feature by heapify. """
-        heap = Heap()
-        heap.set_arr([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
+        heap = Heap([16, 4, 10, 14, 7, 9, 3, 2, 8, 1])
         expected_arr = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
         heap._max_heapify(1)  # heapify with element 4
         self.assertEqual(heap.arr, expected_arr)
 
     def test_create_heap(self):
         """ TestHeap: create max-heap from array. """
-        heap = Heap()
-        heap.set_arr([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+        heap = Heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
         expected_arr = [16, 14, 10, 8, 7, 9, 3, 2, 4, 1]
-        heap.build_heap()  # heapify with element 4
+        heap.build_heap()
+        self.assertEqual(heap.arr, expected_arr)
+
+    def test_heapsort(self):
+        """ TestHeap: sort array by heapsort. """
+        heap = Heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
+        expected_arr = [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
+        heap.sort()
         self.assertEqual(heap.arr, expected_arr)
